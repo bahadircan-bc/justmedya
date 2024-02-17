f9587672556aa8fdf8a5a7cd88d756df2a957b2b
Bahadır Can <bahadircan.bc@gmail.com>
2024-02-17 18:35:48 +0300

    added favicon, fixed page title

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'justmedya - Home',
+  description: 'justmedya - Home',
 }
 
 import Hero from '@/components/hero'
